ea9d1aa9a8cea2436086793b4dd31ea237c5a95f
leng <leng.ling@raythinktech.com>
2022-06-09 12:04:06 +0800

    initial upload.

---
+++ Scan/Scan/scan.cpp
@@ -0,0 +1,136 @@
+#include "scan.h"
+#include "ui_scan.h"
+#include<QDebug>
+#include<QJsonObject>
+#include<QJsonDocument>
+
+Scan::Scan(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Scan)
+{
+    ui->setupUi(this);
+    manager = new QNetworkAccessManager(this);
+    connect(manager,&QNetworkAccessManager::finished,this,&Scan::postBack);//通信完成后，自动执行getBack
+    post();
+}
+
+Scan::~Scan()
+{
+    delete ui;
+}
+
+QString Scan::getTimeStamp(void)
+{
+    QDateTime dateTime = QDateTime::currentDateTime();
+    // 字符串格式化
+    QString timestamp = dateTime.toString("yyyy-MM-dd hh:mm:ss.zzz");
+    // 转换成时间戳
+    qint64 epochTime = dateTime.toMSecsSinceEpoch();
+
+    QString mstimestamp = tr("%1").arg(epochTime);
+
+    return mstimestamp;
+}
+
+QString Scan::calcMD5(QString source)
+{
+    QString md5;
+    QByteArray ba,bb;
+    QCryptographicHash md(QCryptographicHash::Md5);
+
+    ba.append(source);
+    md.addData(ba);
+    bb = md.result();
+    md5.append(bb.toHex());
+
+    return md5;
+}
+
+void Scan::post()
+{
+    // Json数据
+    QJsonObject json;
+
+#if 0
+    QString t = "1456423044000";
+    QString memberID = "125725728";
+    QString brandID = "1";
+    QString posWebServiceKey = "WUsCxdwuP2k3wwqv";
+    QString md5 = calcMD5(t  + memberID + brandID + posWebServiceKey);
+    qDebug() << md5;
+#endif
+
+#if 1
+    QString posWebServiceKey = "WUsCxdwuP2k3wwqv";
+    QString memberID = "125725728";
+    QString Phone = "0";
+    QString brandID = "258";
+    QString shopCode = "Sino Group	001	Sino Inno Lab";
+    QString t = getTimeStamp();
+    QString h = calcMD5(t + memberID + brandID + posWebServiceKey);
+    QString pos = "smartmore";
+    QString nonce = "1";
+
+    json.insert("memberID", memberID);
+    json.insert("Phone", Phone);
+    json.insert("brandID", brandID);
+    json.insert("shopCode", shopCode);
+    json.insert("t", t);
+    json.insert("h", h);
+    json.insert("pos", pos);
+    json.insert("nonce", nonce);
+
+    qDebug() << json;
+
+    QJsonDocument document;
+    document.setObject(json);
+    QByteArray dataArray = document.toJson(QJsonDocument::Compact);
+
+#if 0
+    // 表单数据
+    QByteArray dataArray;
+    dataArray.append("username=admin&");
+    dataArray.append("password=admin");
+#endif
+
+    // 构造请求
+    QNetworkRequest request;
+    request.setUrl(QUrl("https://uat.storellet.com/storellet/api/pos"));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    // 发送请求
+    manager->post(request, dataArray);
+#endif
+}
+
+
+void Scan::postBack(QNetworkReply* reply)
+{
+    qDebug()<<reply->readAll().data(); //输出所有响应内容
+
+    // 获取响应信息
+    QByteArray bytes = reply->readAll();
+
+    QJsonParseError jsonError;
+    QJsonDocument doucment = QJsonDocument::fromJson(bytes, &jsonError);
+    if (jsonError.error != QJsonParseError::NoError) {
+        qDebug() << QStringLiteral("解析Json失败");
+        return;
+    }
+
+    // 解析Json
+    if (doucment.isObject())
+    {
+        QJsonObject obj = doucment.object();
+        QJsonValue value;
+        if (obj.contains("data"))
+        {
+            value = obj.take("data");
+            if (value.isString())
+            {
+                QString data = value.toString();
+                qDebug() << data;
+            }
+        }
+     }
+}
